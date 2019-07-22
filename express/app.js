d225afb3b7e81c9715714bd8e85d16d9eae76fc8
Mahmoud <opssheeh@gmail.com>
2019-07-22 14:47:38 +0300

    add styling and serve static files

--- express/app.js
+++ express/app.js
@@ -5,14 +5,16 @@ const bodyParser = require('body-parser');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+
 const app = express(); 
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use( (req, res) => {
+app.use((req, res) => {
 	console.log('Enter the 404 middleware')
 	res.status(404).send("<h1>PAGE NOT FOUND</h1>")
 } )
--- express/routes/shop.js
+++ express/routes/shop.js
@@ -1,11 +1,14 @@
 //shop.js
 const express = require('express');
+const path = require('path');
+
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
 	console.log("Enter the / middleware");
-	res.send("<h1>Hello from Express</h1>");
+	res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 } );
 
 module.exports = router;
